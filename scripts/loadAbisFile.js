04d2455bc35e793bc947aa78c8c83fb8c139af45
kchain-solutions <valerio.mellini87@icloud.com>
2023-02-05 19:31:26 +0000

    abi update script

--- scripts/loadAbisFile.js
+++ scripts/loadAbisFile.js
@@ -1,1 +1,28 @@
 const fs = require('fs');
+
+const PATH = "./artifacts/contracts";
+const TARGET_PATH = "./abis"
+
+async function main() {
+    const dirList = fs.readdirSync(PATH);
+    const targetFiles = await Promise.all(dirList.map(async (el) => {
+        try {
+            const tmp = el.split('.');
+            const abiFile = tmp[0] + ".json";
+            const file = fs.readFileSync(PATH + '/' + el + '/' + abiFile);
+            const json = JSON.parse(file);
+            const abiContent = JSON.stringify(json.abi);
+            fs.writeFileSync(TARGET_PATH + "/" + abiFile, abiContent);
+            return abiFile;
+        } catch (error) {
+            return error + " on element " + el;
+        }
+    }));
+    console.log("Result: ", targetFiles);
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
